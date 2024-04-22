c574e5b82b8d2ce3d709b79e99e80ede3c115374
Asrinne Nazari <asse_89@hotmail.com>
2024-04-22 21:16:13 +0200

    starting on a new test, createHtml test!

--- src/ts/movieApp.ts
+++ src/ts/movieApp.ts
@@ -1,3 +1,4 @@
+import { createHtml } from "./createHtml";
 import { IMovie } from "./models/Movie";
 import { getData } from "./services/movieService";
 
@@ -33,23 +34,6 @@ export async function handleSubmit() {
   }
 }
 
-export const createHtml = (movies: IMovie[], container: HTMLDivElement) => {
-  for (let i = 0; i < movies.length; i++) {
-    let movie = document.createElement("div");
-    let title = document.createElement("h3");
-    let img = document.createElement("img");
-
-    movie.classList.add("movie");
-    title.innerHTML = movies[i].Title;
-    img.src = movies[i].Poster;
-    img.alt = movies[i].Title;
-
-    movie.appendChild(title);
-    movie.appendChild(img);
-
-    container.appendChild(movie);
-  }
-};
 
 export const displayNoResult = (container: HTMLDivElement) => {
   let noMessage = document.createElement("p");
